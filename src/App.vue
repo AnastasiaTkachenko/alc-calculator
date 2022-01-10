7ba4c2c458d0995786a08f2e62203bdc67b4ba18
anastasia <anastasitkachenko@gmail.com>
2022-01-10 20:58:50 +0100

    test

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+
+<v-app>
+ <div class="container">
+  <v-btn block color="yellow darken-1">
+    GO TO QUESTIONS
+  </v-btn>
+
+  <v-btn block color="yellow darken-1">
+    GO TO ALCOHOL CALCULATOR
+  </v-btn>
+
+
+ </div>
+</v-app>
+
+
+
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'App',
+
+  components: {
+
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style lang="scss">
+
+
+.container {
+  background-image:url('assets/img/duck.jpg');
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 100vw;
+  height: 100vh;
+
+}
+
+
+
+</style>
