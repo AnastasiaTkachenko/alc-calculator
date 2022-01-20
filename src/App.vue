b3a633ecfb80ccd1b56481ecbcc8f2fb0dbc5f65
anastasia <anastasitkachenko@gmail.com>
2022-01-20 12:54:28 +0100

    game page

--- src/App.vue
+++ src/App.vue
@@ -1,135 +1,29 @@
 <template>
+  <v-app>
+    <v-main>
+<Game />
+      <router-view/>
 
-  <div id="app">
-
-<v-app id="main">
-
- <div class="container">
-
-
-          <!--HEADING -->
-   <h1 class="text-center">
-     FANCY <br> DRINKING?
-   </h1>
-
-
-<!--BUTTONS -->
-
-  <v-btn class="button1" block color="yellow darken-1">
-    GO TO QUESTIONS
-  </v-btn>
-
-  <v-btn class="button2" block color="yellow darken-1">
-    GO TO ALCOHOL CALCULATOR
-  </v-btn>
-
-
-<div class="text">
-   <h2 class="text-center">
-     answer a question and let the universe lead you to the
-   </h2>
-
-   <h2 class="text-center" >
-     🎉  PERFECT DRINK OF THE DAY  🎉
-   </h2>
-</div>
-
-<!--   FOOTER -->
-   <v-footer bottom fixed padless>
-     <v-col
-         class="text-center"
-         cols="12"
-     >
-       {{ new Date().getFullYear() }} — <strong>Cheeers!</strong>
-     </v-col>
-   </v-footer>
-
-
-
-
- </div>
-
-
-
-</v-app>
-
-
-
-
-
-</div>
-
-
-
-
+    </v-main>
+  </v-app>
 </template>
 
 <script>
 
-
-
+import Game from './components/Game'
 export default {
   name: 'App',
-
   components: {
-
+    Game,
   },
 
   data: () => ({
     //
   }),
 };
-
-
-
-
 </script>
 
 <style lang="scss">
-
-@import url('https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap');
-
-
-.container {
-  background-image:url('assets/img/duck.jpg');
-  background-repeat: no-repeat;
-  background-position: center;
-  min-width: 100%;
-  min-height: 100%;
-
-
-
-}
-
-.container .button1 {
-  margin-top: 400px;
-}
-
-.v-btn {
- margin-top: 20px;
-
-}
-
-
-h1 {
-  position: absolute; // for central text we use width:100%; left:0px;
-  top:2em;
-  font-size:4rem;
-  font-family: Lato,sans-serif;
-  width:100%; left:0px;
-  letter-spacing: 20px;
-
-}
-
-h2 {
-  font-family: Lato, sans-serif;
-  position: relative; // using for not pushing everything down
-  top: 5em;
-  margin-top:8px;
-
-
-}
-
 
 
 </style>
---
+++ src/components/Game.vue
@@ -0,0 +1,88 @@
+<template>
+<div id="app">
+  <v-app id="main">
+
+    <div class="container">
+
+
+      <div id="round-botton">
+          <v-row justify="space-around">
+            <v-menu
+                bottom
+                origin="center center"
+                transition="scale-transition"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                    class="mx-2"
+                    fab
+                    dark
+                    large
+                    color="yellow darken-1"
+                    v-bind="attrs"
+                    v-on="on"
+                >
+                  <v-icon dark>
+                    mdi-plus
+                  </v-icon>
+                </v-btn>
+              </template>
+
+              <v-list>
+                <v-list-item
+                    v-for="row in 2"
+                    :key="row"
+                    @click="() => {}"
+                >
+<v-text-field to="/Home" v-text="'MAIN'"
+                  </v-text-field>
+
+                  <!--                  <v-list-item-title to="/Home" v-text="'MAIN'"></v-list-item-title>-->
+
+                </v-list-item>
+              </v-list>
+            </v-menu>
+
+          </v-row>
+
+
+      </div>
+
+    </div>
+
+
+
+
+
+
+    </v-app>
+
+
+
+</div>
+</template>
+
+<script>
+
+// new Vue({
+//   el: '#app',
+//   vuetify: new Vuetify(),
+//   data: () => ({
+//     items: [
+//       { title: 'Click Me' },
+//       { title: 'Click Me' },
+//       { title: 'Click Me' },
+//       { title: 'Click Me 2' },
+//     ],
+//   }),
+// })
+
+export default {
+  name: "Game"
+
+}
+</script>
+
+<style scoped>
+
+</style>
